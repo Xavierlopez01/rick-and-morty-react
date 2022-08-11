7b3ac2045632c8c10d2b765efcb538b959c07673
Xavier Jesús López Tovar <xavierjesus1986@gmail.com>
2022-08-11 18:43:32 -0400

    act

--- src/components/CardCharacters.jsx
+++ src/components/CardCharacters.jsx
@@ -29,7 +29,7 @@ const CardCharacters = ({ character }) => {
     }, [page])
     if (loading) {
         return (
-            <img src="../../public/loading.png" alt="" />
+            <img src="../public/loading.png" alt="" />
         )
     }
     return (
